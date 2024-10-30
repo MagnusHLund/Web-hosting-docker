474f94fb6eb682d8c36fd7da5fe8215cb6052a09
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2024-10-30 19:45:57 +0100

    refactor: :recycle: Reworked the api router

--- api/src/managers/ApiRouter.php
+++ api/src/managers/ApiRouter.php
@@ -2,14 +2,15 @@
 
 namespace MagZilla\Api\Managers;
 
-use MagZilla\Api\Controllers\AuthenticationController;
-use MagZilla\Api\Controllers\ServiceController;
-use MagZilla\Api\Controllers\UserController;
 use Psr\Container\ContainerInterface;
+use MagZilla\Api\Services\ResponseService;
+use MagZilla\Api\Controllers\UserController;
+use MagZilla\Api\Controllers\ServiceController;
+use MagZilla\Api\Controllers\AuthenticationController;
 
 class ApiRouter
 {
-    private $routes = array();
+    private $routes = [];
 
     public function __construct(ContainerInterface $container)
     {
@@ -18,42 +19,36 @@ class ApiRouter
         $userController = $container->get(UserController::class);
 
         $this->routes = [
-            ["PUT",   "/api/auth/updatePassword",           [$authenticationController, "updatePassword"]],
-            ["POST",   "/api/auth/login",                   [$authenticationController, "login"]],
-            ["POST",   "/api/auth/logout",                  [$authenticationController, "logout"]],
+            ["PUT",   "/api/auth/updatePassword",           $authenticationController, "updatePassword"],
+            ["POST",   "/api/auth/login",                   $authenticationController, "login"],
+            ["POST",   "/api/auth/logout",                  $authenticationController, "logout"],
 
-            ["POST",   "/api/services/addService",          [$serviceController, "addService"]],
-            ["GET",    "/api/services/getServiceDetails",   [$serviceController, "getServiceDetails"]],
-            ["GET",    "/api/services/getServices",         [$serviceController, "getServices"]],
-            ["PUT",    "/api/services/updateService",       [$serviceController, "updateService"]],
-            ["PUT",    "/api/services/updateServiceSource", [$serviceController, "updateServiceSource"]],
-            ["DELETE", "/api/services/deleteService",       [$serviceController, "deleteService"]],
+            ["POST",   "/api/services/addService",          $serviceController,        "addService"],
+            ["GET",    "/api/services/getServiceDetails",   $serviceController,        "getServiceDetails"],
+            ["GET",    "/api/services/getServices",         $serviceController,        "getServices"],
+            ["PUT",    "/api/services/updateService",       $serviceController,        "updateService"],
+            ["PUT",    "/api/services/updateServiceSource", $serviceController,        "updateServiceSource"],
+            ["DELETE", "/api/services/deleteService",       $serviceController,        "deleteService"],
 
-            ["POST",   "/api/users/addUser",                [$userController, "addUser"]],
-            ["GET",    "/api/users/getSettings",            [$userController, "getSettings"]],
-            ["GET",    "/api/users/getUsers",               [$userController, "getUsers"]],
-            ["PUT",    "/api/users/updateSetting",          [$userController, "updateSetting"]],
-            ["PUT",    "/api/users/updateUser",             [$userController, "updateUser"]],
-            ["DELETE", "/api/users/deleteUser",             [$userController, "deleteUser"]],
+            ["POST",   "/api/users/addUser",                $userController,           "addUser"],
+            ["GET",    "/api/users/getSettings",            $userController,           "getSettings"],
+            ["GET",    "/api/users/getUsers",               $userController,           "getUsers"],
+            ["PUT",    "/api/users/updateSetting",          $userController,           "updateSetting"],
+            ["PUT",    "/api/users/updateUser",             $userController,           "updateUser"],
+            ["DELETE", "/api/users/deleteUser",             $userController,           "deleteUser"],
         ];
     }
 
     public function handleRequest($method, $path, $requestBody)
     {
         foreach ($this->routes as $route) {
-            $routeMethod = $route[0];
-            $routePath   = $route[1];
-            $handler     = $route[2];
-            $params      = $route[3] ?? [];
-
+            [$routeMethod, $routePath, $controller, $action] = $route;
             if ($method === $routeMethod && $path === $routePath) {
-                call_user_func_array([$handler[0], $handler[1]], [$requestBody, $params]); // TODO: Can this be improved?
+                call_user_func([$controller, $action], $requestBody);
                 return;
             }
         }
 
-        http_response_code(404);
-        echo json_encode(["error" => "This route does not exist!"]);
-        exit;
+        ResponseService::getInstance()->sendResponse("This route does not exist!", 404);
     }
 }
